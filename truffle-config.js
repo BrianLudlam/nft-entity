2f7fefe44cfa66a036bd789ed696bc21822438e6
brianin802 <brianludlam@gmail.com>
2019-06-19 19:09:14 -0400

    Cleaned up truffle-config.

--- truffle-config.js
+++ truffle-config.js
@@ -17,13 +17,11 @@
  * phrase from a file you've .gitignored so it doesn't accidentally become public.
  *
  */
-
 const HDWalletProvider = require('truffle-hdwallet-provider');
 const fs = require('fs');
-const from = fs.readFileSync(".account").toString().trim();
-const mnemonic = fs.readFileSync(".mnemonic").toString().trim();
-const infura = fs.readFileSync(".infura").toString().trim();
-const provider = () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/${infura}`);
+const devAccount = fs.readFileSync(".account").toString().trim();
+const devMnemonic = fs.readFileSync(".mnemonic").toString().trim();
+const infuraKey = fs.readFileSync(".infura").toString().trim();
 
 module.exports = {
   /**
@@ -35,14 +33,14 @@ module.exports = {
    *
    * $ truffle test --network <network-name>
    */
-
   networks: {
     // Useful for testing. The `development` name is special - truffle uses it by default
     // if it's defined here and no other network is specified at the command line.
     // You should run a client (like ganache-cli, geth or parity) in a separate terminal
     // tab if you use this network and you must also set the `host`, `port` and `network_id`
     // options below to some value.
-    //
+    
+    //Local testing Ganache settings
     development: {
         host: "127.0.0.1",     // Localhost (default: none)
         port: 8545,            // Standard Ethereum port (default: none)
@@ -50,53 +48,26 @@ module.exports = {
         gas: 6000000,
         gasPrice: 5000000000,  // 5 gwei 
     },
-
-    // Another network with more advanced options...
-    // advanced: {
-      // port: 8777,             // Custom port
-      // network_id: 1342,       // Custom network
-      // gas: 8500000,           // Gas sent with each transaction (default: ~6700000)
-      // gasPrice: 20000000000,  // 20 gwei (in wei) (default: 100 gwei)
-      // from: <address>,        // Account to send txs from (default: accounts[0])
-      // websockets: true        // Enable EventEmitter interface for web3 (default: false)
-    // },
-
-    // Useful for deploying to a public network.
-    // NB: It's important to wrap the provider as a function.
+    //Remote testing Ropsten settings
     ropsten: {
-      provider,
-      from,
+      provider: () => new HDWalletProvider(devMnemonic, `https://ropsten.infura.io/v3/${infuraKey}`),
+      from: devAccount,
       network_id: 3,       // Ropsten's id
       gas: 4000000,        // Ropsten has a lower block limit than mainnet
       confirmations: 5,    // # of confs to wait between deployments. (default: 0)
       timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-    //  skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+      //skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
     },
-
-    // Useful for private networks
-    // private: {
-      // provider: () => new HDWalletProvider(mnemonic, `https://network.io`),
-      // network_id: 2111,   // This network is yours, in the cloud.
-      // production: true    // Treats this network as if it was a public net. (default: false)
-    // }
   },
-
-  // Set default mocha options here, use special reporters etc.
-  mocha: {
-    // timeout: 100000
-  },
-
-  // Configure your compilers
+  //Complier settings
   compilers: {
     solc: {
-      version: "0.5.8",    // Fetch exact version from solc-bin (default: truffle's version)
-      // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
-      settings: {          // See the solidity docs for advice about optimization and evmVersion
+      version: "0.5.8",    
+      settings: {         
         optimizer: {
           enabled: true,
           runs: 10000
-        },
-        //evmVersion: "CONTANTINOPLE"
+        }
       }
     }
   }
